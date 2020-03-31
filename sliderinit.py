8d569e27899dd5fd4ee05b57fcd852ed745b749f
tannerwilson23 <37657384+tannerwilson23@users.noreply.github.com>
2020-03-31 12:36:30 +1100

    Add files via upload

--- sliderinit.py
+++ sliderinit.py
@@ -1,16 +1,16 @@
-
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider, Button, RadioButtons
-
-#import lightkurve as lk
-import scipy.ndimage
-
-import pymc3 as pm
-import theano.tensor as tt
-from exoplanet.gp import terms, GP
-
-import exoplanet as xo
+#
+#import numpy as np
+#import matplotlib.pyplot as plt
+#from matplotlib.widgets import Slider, Button, RadioButtons
+#
+##import lightkurve as lk
+#import scipy.ndimage
+#
+#import pymc3 as pm
+#import theano.tensor as tt
+#from exoplanet.gp import terms, GP
+#
+#import exoplanet as xo
 
 
 import numpy as np
@@ -50,79 +50,76 @@ plt.show()
 plt.figure()
 plt.plot(time, ts.real, 'b-', time, ts.imag, 'r--')
 plt.show()
-t = np.sort(
-    np.append(np.random.uniform(0, 12, 800), np.random.uniform(15, 30, 1000))
-)
-print(t)
+
 #yerr = 0.005* np.ones_like(t)
 
 
 
 
-def run_gp_single(Sgv,wgv,S1v,w1v,Q1v):
-
-    with pm.Model() as model:
-
-
-        logSg = pm.Normal("logSg", mu=Sgv, sigma= 100.0, testval=Sgv)
-        logwg = pm.Normal("logwg", mu = wgv, sigma = 100.0, testval=wgv)
-        logS1 = pm.Normal("logS1", mu=S1v, sigma=100.0, testval=S1v)
-        logw1 = pm.Normal("logw1", mu =w1v, sigma = 100.0, testval=w1v)
-        logQ1 = pm.Normal("logQ1", mu=Q1v, sigma=100.0, testval=Q1v)
-
-
-        # Set up the kernel an GP
-        bg_kernel = terms.SHOTerm(log_S0=logSg, log_w0=logwg, Q=1.0 / np.sqrt(2))
-        star_kernel1 = terms.SHOTerm(log_S0=logS1, log_w0=logw1, log_Q=logQ1)
-        kernel = star_kernel1 + bg_kernel
-
-        gp = GP(kernel, t, yerr)
-        #gp_star1 = GP(star_kernel1, t, yerr ** 2 + pm.math.exp(logs2))
-        #gp_bg = GP(bg_kernel, t, yerr ** 2 + pm.math.exp(logs2))
-        
-        gp_star1 = GP(star_kernel1, t, yerr)
-        gp_bg = GP(bg_kernel, t, yerr, mean = 0.)
-
-        # Condition the GP on the observations and add the marginal likelihood
-        # to the model
-        gp.marginal("gp")
-        
-        
-    with model:
-        val = gp.kernel.psd(omega)
-
-        psd_init = xo.eval_in_model(val)
-
-        bg_val = gp_bg.kernel.psd(omega)
-        star_val_1 = gp_star1.kernel.psd(omega)
-
-
-
-        bg_psd_init = xo.eval_in_model(bg_val)
-        star_1_psd_init = xo.eval_in_model(star_val_1)
-
-
-    #     print('done_init_plot')
-    
-        map_soln = pm.find_MAP(start=model.test_point)
-
-
-        map_soln = model.test_point
-        print(model.test_point)
-
-        psd_final = xo.eval_in_model(gp.kernel.psd(omega),map_soln)
-
-
-        bg_psd_fin = xo.eval_in_model(bg_val,map_soln)
-        star_1_psd_fin = xo.eval_in_model(star_val_1,map_soln)
-        
-
-        
-
-        
-    return psd_init, star_1_psd_init, bg_psd_init , psd_final, star_1_psd_fin, bg_psd_fin, map_soln
-
-
+#def run_gp_single(Sgv,wgv,S1v,w1v,Q1v):
+#
+#    with pm.Model() as model:
+#
+#
+#        logSg = pm.Normal("logSg", mu=Sgv, sigma= 100.0, testval=Sgv)
+#        logwg = pm.Normal("logwg", mu = wgv, sigma = 100.0, testval=wgv)
+#        logS1 = pm.Normal("logS1", mu=S1v, sigma=100.0, testval=S1v)
+#        logw1 = pm.Normal("logw1", mu =w1v, sigma = 100.0, testval=w1v)
+#        logQ1 = pm.Normal("logQ1", mu=Q1v, sigma=100.0, testval=Q1v)
+#
+#
+#        # Set up the kernel an GP
+#        bg_kernel = terms.SHOTerm(log_S0=logSg, log_w0=logwg, Q=1.0 / np.sqrt(2))
+#        star_kernel1 = terms.SHOTerm(log_S0=logS1, log_w0=logw1, log_Q=logQ1)
+#        kernel = star_kernel1 + bg_kernel
+#
+#        gp = GP(kernel, t, yerr)
+#        #gp_star1 = GP(star_kernel1, t, yerr ** 2 + pm.math.exp(logs2))
+#        #gp_bg = GP(bg_kernel, t, yerr ** 2 + pm.math.exp(logs2))
+#
+#        gp_star1 = GP(star_kernel1, t, yerr)
+#        gp_bg = GP(bg_kernel, t, yerr, mean = 0.)
+#
+#        # Condition the GP on the observations and add the marginal likelihood
+#        # to the model
+#        gp.marginal("gp")
+#
+#
+#    with model:
+#        val = gp.kernel.psd(omega)
+#
+#        psd_init = xo.eval_in_model(val)
+#
+#        bg_val = gp_bg.kernel.psd(omega)
+#        star_val_1 = gp_star1.kernel.psd(omega)
+#
+#
+#
+#        bg_psd_init = xo.eval_in_model(bg_val)
+#        star_1_psd_init = xo.eval_in_model(star_val_1)
+#
+#
+#    #     print('done_init_plot')
+#
+#        map_soln = pm.find_MAP(start=model.test_point)
+#
+#
+#        map_soln = model.test_point
+#        print(model.test_point)
+#
+#        psd_final = xo.eval_in_model(gp.kernel.psd(omega),map_soln)
+#
+#
+#        bg_psd_fin = xo.eval_in_model(bg_val,map_soln)
+#        star_1_psd_fin = xo.eval_in_model(star_val_1,map_soln)
+#
+#
+#
+#
+#
+#    return psd_init, star_1_psd_init, bg_psd_init , psd_final, star_1_psd_fin, bg_psd_fin, map_soln
+#
+#
 
 
 #a,b,c,d,e,f,g = run_gp_single(Sg0,wg0,S10,w10,Q10)
